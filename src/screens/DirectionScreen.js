9235b71be524cdf1fe2a525c2c87cd35aef58c2b
Marc <booooza@gmail.com>
2018-04-17 00:29:12 +0200

    Update directions screen

--- src/screens/DirectionScreen.js
+++ src/screens/DirectionScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
-import { Location, Permissions } from 'expo';
+import { View, Text, SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+import { Location, Permissions, MapView } from 'expo';
 
 import DirectionService from '../services/directions';
 import Map from '../components/Map';
@@ -10,25 +10,22 @@ const deltas = {
     longitudeDelta: 0.0421
 };
 
-// const { latitude, longitude, title, type } = this.props.navigation.state.params;
+// const { params } = this.props.navigation.state.params;
 
 class DirectionScreen extends Component {
 	state = {
         region: null,
         errorMessage: null,
-        coords: [{
-            latitude:-6.270565,
-            longitude:106.759550,
-        }]
+        coords: []
 	};
 
 	componentWillMount() {
-		this.getLocationAsync();
+        this.getLocationAsync();
 	}
 
-    getBikeData = async () => {
-        const places = await BikeService.getData();
-        this.setState({ places });
+    getDirections = async () => {
+        const coords = await DirectionService.getData("40.1884979, 29.061018", "41.0082,28.9784");
+        this.setState({ coords: coords });
     };
 
 	getLocationAsync = async () => {
@@ -46,30 +43,34 @@ class DirectionScreen extends Component {
           ...deltas
         };
         await this.setState({ region });
-        await this.getBikeData();
+        await this.getDirections();
 	};
 
 	render() {
-        const { region, places } = this.state;
-		if (!places) {
+        const { region, coords } = this.state;
+		if (!coords) {
 			return (
 				<View>
 					<Text>Waiting...</Text>
 				</View>
 			);
 		}
-
+        console.log(JSON.stringify(this.state.coords))
 		return (
             <SafeAreaView style={styles.container}>
-                <Map
+                <MapView
+                style={styles.map}
                 initialRegion={region}
-                />
+                showsUserLocation
+                showsMyLocationButton
+                >
                 <MapView.Polyline
-                    coordinates={this.state.coords}
+                    style={styles.map}
+                    coordinates={coords}
                     strokeWidth={2}
-                    strokeColor="red"/>
-
+                    strokeColor="red"
                 />
+                </MapView>
             </SafeAreaView>
 		);
 	}
@@ -79,8 +80,19 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		justifyContent: 'flex-start'
-	}
+        justifyContent: 'flex-start',
+        width: '100%',
+		height: '100%'
+    },
+    map: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height
+      },
 });
 
 export default DirectionScreen;
