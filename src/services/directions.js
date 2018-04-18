f30c5b75ece3b09a33c0df093041028d7229302e
Marc <booooza@gmail.com>
2018-04-18 14:57:54 +0200

    Add back directions service with api key from .env file

---
+++ src/services/directions.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import Polyline from '@mapbox/polyline';
+
+const api = axios.create({
+  baseURL: 'https://maps.googleapis.com',
+});
+
+const getData = (startLoc, destinationLoc) => {
+  const APIKEY = process.env.GOOGLE_DIRECTIONS_APIKEY
+  const mode = 'bicycling'
+  return api
+    .get(`/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }&key=${APIKEY}&mode=${mode}`)
+    .then(res => {
+        return Polyline.decode(res.data.routes[0].overview_polyline.points);
+    })
+    .then((points) => {
+        let coords = points.map((point, index) => {
+            return {
+                latitude : point[0],
+                longitude : point[1]
+            };
+        })
+        return coords
+    })
+    
+    .catch(error => console.error(error));
+};
+
+export default {
+    getData
+};
