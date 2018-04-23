045b6daed370cbbef1ced9f7c6e0ce84689dbe5f
Marc <booooza@gmail.com>
2018-04-23 22:49:03 +0200

    Remove unused imports

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Tabs } from './src/config/router';
-import { StacksOverTabs } from './src/config/router';
 import { Root } from './src/config/router';
 
 class App extends React.Component {
