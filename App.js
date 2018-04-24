ac3b3f137088789f0cb1727a348bf37e11bd014a
Marc <booooza@gmail.com>
2018-04-24 22:42:42 +0200

    Implement AppLoading async data caching during loading screen

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import { AppRegistry, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Root } from './src/config/router';
-let data = require('./assets/velopumpstation.json'); 
+import { Asset, AppLoading } from 'expo';
+
 
 class App extends React.Component {
 
+  state = {
+    isReady: false,
+  };
+  
   render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    let data = require('./assets/velopumpstation.json'); 
     return (
         <Root 
         screenProps={{data: data}}
         />
     );
   }
+
+  async _cacheResourcesAsync() {
+    const files = [
+      require('./assets/velopumpstation.json'),
+    ];
+
+    const cacheFiles = files.map((file) => {
+      return Asset.fromModule(file).downloadAsync();
+    });
+    return Promise.all(cacheFiles)
+  }
+
 }
 
 export default App;
