2de60a2ca688b7bcd43bf41bedd3d35b6eca3299
Marc <booooza@gmail.com>
2018-04-24 14:15:26 +0200

    Add API data to local file and pass down via screenProps

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Root } from './src/config/router';
+let data = require('./src/data/velopumpstation.json'); 
 
 class App extends React.Component {
 
   render() {
     return (
-        <Root />
+        <Root 
+        screenProps={{data: data}}
+        />
     );
   }
 }
 
 export default App;
-
+AppRegistry.registerComponent('App', () => App);
